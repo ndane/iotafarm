6453d6debd5647c19963a5309f832dee01aac501
Nathan Dane <nathan.dane@pushdr.com>
2017-11-21 13:58:53 +0000

    Handle seed parameter

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all:
 	rm -rf build
 	mkdir -v build
-	gcc main.c seed_gen.c -o build/seed
+	gcc main.c seed_gen.c -o build/farm
 
 clean:
 	rm -rf build
--- main.c
+++ main.c
@@ -1,10 +1,26 @@
 #include "seed_gen.h"
 
 #include <stdio.h>
+#include <string.h>
+
+void seed();
 
 int main(int argc, char **argv) {
+    if (argc == 1) {
+        seed();
+    }
+
+    for (int i = 0; i < argc; ++i) {
+        if (strcmp("seed", argv[i]) == 0) {
+            seed();
+        }
+    }
+
+    return 0;
+}
+
+void seed() {
     char seed[81];
     seed_gen(seed, 81);
-    printf("%s\n", seed);
-    return 0;
+    puts(seed);
 }
